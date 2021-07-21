8908085f8cc65f3e9608011326129e307b1b30ca
Illia Baievskyi <Illia.Baievskyi@tui.co.uk>
2021-07-21 10:48:59 +0300

    Интеграция с кастомным провайдером на примере Auth0. Java configuration. Использование access token'a

--- src/main/java/solutions/activx/oauth2workshop/HomeController.java
+++ src/main/java/solutions/activx/oauth2workshop/HomeController.java
@@ -1,18 +1,45 @@
 package solutions.activx.oauth2workshop;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
 
 @RestController
+@RequiredArgsConstructor
 public class HomeController {
+    private final RestTemplate restTemplate;
+
+
     @GetMapping
     public String mainPage(Authentication principal) {
         return "<h1>Welcome, " + principal.getName() + "</h1>";
     }
 
     @GetMapping("/auth")
-    public Authentication getAuthentication(Authentication authentication){
+    public Authentication getAuthentication(Authentication authentication) {
         return authentication;
+    }
+
+    /**
+     * @param authorizedClient с помощью аннотации {@link RegisteredOAuth2AuthorizedClient } мы достаем authorizedClient
+     *                         что по сути является агрегацией {@link ClientRegistration } и {@link OAuth2AccessToken}
+     *                         указав при этом registration id "my-app"
+     */
+    @GetMapping("/external")
+    public Map getExternal(@RegisteredOAuth2AuthorizedClient("my-app") OAuth2AuthorizedClient authorizedClient) {
+        var uri = authorizedClient
+                .getClientRegistration()
+                .getProviderDetails()
+                .getUserInfoEndpoint()
+                .getUri();
+        return restTemplate.getForObject(uri, Map.class);
     }
 }
--- src/main/java/solutions/activx/oauth2workshop/SecurityConfiguration.java
+++ src/main/java/solutions/activx/oauth2workshop/SecurityConfiguration.java
@@ -1,16 +1,22 @@
 package solutions.activx.oauth2workshop;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 @EnableWebSecurity
@@ -39,13 +45,36 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                         .scope("openid", "profile", "email")
                         .redirectUri("{baseUrl}/login/oauth2/code/{registrationId}")
 
+                        //Параметры провайдера
                         .authorizationUri("https://activx.auth0.com/authorize")
                         .tokenUri("https://activx.auth0.com/oauth/token")
                         .jwkSetUri("https://activx.auth0.com/.well-known/jwks.json")
+                        .userInfoUri("https://activx.auth0.com/userinfo")
 
                         .userNameAttributeName("nickname")
                         .build()
         );
     }
 
+    /**
+     * Добавляем аксесс токен в исходящие запросы
+     * @param clientService отвечает за получение агрегации между access/refresh token и
+     *                    client registration аутентифицированного пользователя
+     */
+    @Bean
+    public RestTemplate restTemplate(OAuth2AuthorizedClientService clientService) {
+        // Добавляем интерцептор в рест темплейт
+        return new RestTemplateBuilder().interceptors((request, bytes, execution) -> {
+            // Достаем текущий объект аутентификации
+            var auth = ((OAuth2AuthenticationToken) SecurityContextHolder.getContext().getAuthentication());
+
+            // Достаем агрегацию client registration и access token
+            var authorizedClient = clientService.loadAuthorizedClient(auth.getAuthorizedClientRegistrationId(), auth.getName());
+
+            //Добавляем токен в хедеры
+            request.getHeaders().add(HttpHeaders.AUTHORIZATION, "Bearer ".concat(authorizedClient.getAccessToken().getTokenValue()));
+
+            return execution.execute(request, bytes);
+        }).build();
+    }
 }
